87845578c1125420b2f2f1a93d09f4fc19f49272
Peter Hayes <peter.k.hayes@gmail.com>
2017-02-28 09:25:33 -0800

    fix up some lodash stuff

--- index.js
+++ index.js
@@ -20,31 +20,28 @@ Promise.map([ADOPTION_PAGE, `${ADOPTION_PAGE}?page=1`, `${ADOPTION_PAGE}?page=2`
       .map((i, tag) => `${SFSPCA_BASE}/${tag.attribs.href}`)
       .toArray();
   })
-  // flat cats before fat cats
+  // Flat cats before fat cats
   .then(flatten)
   // NO DOUBLE CATS
   .then(uniq)
-  .map((urls) => {
-    const fetches = urls.map((url) => {
-      return request.get(url)
-        // SPCA sometimes returns 403s for some cats, ignore this.
-        .catch((err) => err)
-        .then((catPage) => {
-          const name = /\<h1\>([a-zA-Z]+)\<\/h1\>/.exec(catPage)[1];
-          const lbs = Number(/(\d+)lbs\./.exec(catPage)[1]);
-          const oz = Number(/(\d+)oz\./.exec(catPage)[1]);
-          const isFemale = /Female/.test(catPage);
+  .map((url) => {
+    return request.get(url)
+      // SPCA sometimes returns 403s for some cats, ignore this.
+      .catch((err) => err)
+      .then((catPage) => {
+        const name = /\<h1\>([a-zA-Z]+)\<\/h1\>/.exec(catPage)[1];
+        const lbs = Number(/(\d+)lbs\./.exec(catPage)[1]);
+        const oz = Number(/(\d+)oz\./.exec(catPage)[1]);
+        const isFemale = /Female/.test(catPage);
 
-          console.log("Weighing cat:", name);
-          return {name, lbs, oz, isFemale, url}
-        })
-        // Null for cats that cannot be parsed.
-        .catch(() => null);
-    });
-    return Promise.all(fetches);
+        console.log("Weighing cat:", name);
+        return {name, lbs, oz, isFemale, url}
+      })
+      // Null for cats that cannot be parsed.
+      .catch(() => null);
   })
   // Filter out unparsable cats.
-  .filter(compact)
+  .then(compact)
   .then((cats) => {
     let fattestCat = {lbs: 0, oz: 0};
     cats.forEach((cat) => {
