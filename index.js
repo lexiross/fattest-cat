74e1a9f7d506534cb23c89aaa36d7cae01fcd40e
Peter Hayes <peter.k.hayes@gmail.com>
2017-02-28 09:15:44 -0800

    use lodash more consistently

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
 const Promise = require("bluebird");
-const {flatten} = require("lodash");
 const request = require("request-promise");
 const cheerio = require("cheerio");
 const exec    = require("child_process").execSync;
 
+const {flatten, uniq, compact} = require("lodash");
+
 const SFSPCA_BASE = "https://www.sfspca.org"
 const ADOPTION_PAGE = `${SFSPCA_BASE}/adoptions/cats`;
-const CAT_URL_REGEX = /adoptions\/pet-details\/\d+/;
-
 
 console.log("Accessing San Francisco SPCA (Cat Department)...");
+
 // Assume no more than 3 pages of cats
 Promise.map([ADOPTION_PAGE, `${ADOPTION_PAGE}?page=1`, `${ADOPTION_PAGE}?page=2`], request.get)
   .tap(() => console.log("Cat information system accessed. Beginning weighing process..."))
   .map((adoptionsPage) => {
-    const urls = cheerio(adoptionsPage)
+    return cheerio(adoptionsPage)
       .find("a")
       .filter((i, tag) => tag.attribs.href && tag.attribs.href.match(/adoptions\/pet-details\/\d+/))
       .map((i, tag) => `${SFSPCA_BASE}/${tag.attribs.href}`)
       .toArray();
-
-    // Remove any duplicates
-    return urls.filter((url, i) => urls.indexOf(url) === i);
-  })
-  .map((urls) => {
-    const fetches = urls.map((url) => {
-        return request.get(url)
-          // SPCA sometimes returns 403s for some cats, ignore this.
-          .catch((err) => err)
-          .then((catPage) => {
-            const name = /\<h1\>([a-zA-Z]+)\<\/h1\>/.exec(catPage)[1];
-            const lbs = Number(/(\d+)lbs\./.exec(catPage)[1]);
-            const oz = Number(/(\d+)oz\./.exec(catPage)[1]);
-            const isFemale = /Female/.test(catPage);
-
-            console.log("Weighing cat:", name);
-            return {name, lbs, oz, isFemale, url}
-          })
-          // Null for cats that cannot be parsed.
-          .catch(() => null);
-    });
-    return Promise.all(fetches);
   })
   // flat cats before fat cats
   .then(flatten)
+  // NO DOUBLE CATS
+  .then(uniq)
+  .map((urls) => {
+    const fetches = urls.map((url) => {
+      return request.get(url)
+        // SPCA sometimes returns 403s for some cats, ignore this.
+        .catch((err) => err)
+        .then((catPage) => {
+          const name = /\<h1\>([a-zA-Z]+)\<\/h1\>/.exec(catPage)[1];
+          const lbs = Number(/(\d+)lbs\./.exec(catPage)[1]);
+          const oz = Number(/(\d+)oz\./.exec(catPage)[1]);
+          const isFemale = /Female/.test(catPage);
+
+          console.log("Weighing cat:", name);
+          return {name, lbs, oz, isFemale, url}
+        })
+        // Null for cats that cannot be parsed.
+        .catch(() => null);
+    });
+    return Promise.all(fetches);
+  })
   // Filter out unparsable cats.
-  .filter(Boolean)
+  .filter(compact)
   .then((cats) => {
     let fattestCat = {lbs: 0, oz: 0};
     cats.forEach((cat) => {
