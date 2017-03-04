31054676c49a8a964f23d178ef3bafb95b57d651
Peter Hayes <peter.k.hayes@gmail.com>
2017-03-03 16:57:01 -0800

    streamline reporting when multiple cats tie

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
-const request         = require("request-promise");
-const opener          = require("opener");
-const Promise         = require("bluebird");
-const cheerio         = require("cheerio");
-const {uniq, compact} = require("lodash");
-const colors          = require("colors");
+const _       = require("lodash");
+const request = require("request-promise");
+const opener  = require("opener");
+const Promise = require("bluebird");
+const cheerio = require("cheerio");
+
+require("colors");
+
+
 
 const SFSPCA_BASE = "https://www.sfspca.org"
 const ADOPTION_PAGE = `${SFSPCA_BASE}/adoptions/cats`;
@@ -30,16 +33,10 @@ const fetchCatsHelper = Promise.method((pageNumber, catsSoFar) => {
       return catsSoFar;
     });
 });
-const fetchCats = () => fetchCatsHelper(0, []);
 
 console.log("Accessing San Francisco SPCA (Cat Department)...");
-
-colors.setTheme({
-  output: ["yellow", "bold"],
-});
-
-fetchCats()
-  .then(uniq) // NO DOUBLE CATS
+fetchCatsHelper(0, [])
+  .then(_.uniq) // NO DOUBLE CATS
   .tap((cats) => console.log(`Cat information system accessed. ${cats.length} cats found. Beginning weighing process...`))
   .map((url) => {
     return request.get(url)
@@ -48,50 +45,37 @@ fetchCats()
       .then((catPage) => {
         const $ = cheerio.load(catPage);
         const name = $(".field-name-title h1").text();
-        const weight = $(".field-name-field-animal-weight .field-item").text();
-        const lbs = Number(/(\d+)lbs\./.exec(weight)[1]);
-        const oz = /(\d+)oz\./.test(weight) ? Number(/(\d+)oz\./.exec(weight)[1]) : 0;
+        const weightText = $(".field-name-field-animal-weight .field-item").text();
+        const lbs = Number(/(\d+)lbs\./.exec(weightText)[1]);
+        const oz = /(\d+)oz\./.test(weightText) ? Number(/(\d+)oz\./.exec(weightText)[1]) : 0;
+        const weight = 16 * lbs + oz;
         const isFemale = $(".field-name-field-gender .field-item").text().trim() === "Female";
 
-        console.log("Weighing cat: %s", colors.green(name));
-        return {name, lbs, oz, isFemale, url}
+        console.log("Weighing cat: %s", name.green);
+        return {name, lbs, oz, weight, isFemale, url}
       })
       // Null for cats that cannot be parsed.
       .catch(() => {});
   })
   // Filter out unparsable cats.
-  .then(compact)
+  .then(_.compact)
   .then((cats) => {
-    let fattestCats = [{lbs: 0, oz: 0}];
-    cats.forEach((cat) => {
-      // New fattest cat
-      if (cat.lbs > fattestCats[0].lbs || (cat.lbs === fattestCats[0].lbs && cat.oz > fattestCats[0].oz)) {
-        fattestCats = [cat];
-
-        // tie
-      } else if (cat.lbs === fattestCats[0].lbs && cat.oz === fattestCats[0].oz) {
-        fattestCats.push(cat);
-      }
-    });
-
-    // Only display up to 10 fattest cats
-    fattestCats = fattestCats.slice(0, 10);
-
-    //Decide whether to display a single cat or multiple
-    let winnerDesc;
-    if (fattestCats.length === 1) {
-      winnerDesc = `cat is ${colors.green.underline(fattestCats[0].name)}. ${(fattestCats[0].isFemale ? "She" : "He")} weighs`.output;
-    } else {
-      winnerDesc = `cats are ${fattestCats.map((cat) => colors.green.underline(cat.name)).join(", ")}. They each weigh`.output;
-
-      const pos = winnerDesc.lastIndexOf(",");
-      winnerDesc = winnerDesc.substring(0, pos) + " and" + winnerDesc.substring(pos + 1);
+    if (cats.length === 0) {
+      console.log("No cats found. It is a sad day.".red.bold);
+      return;
     }
-    console.log(`The fattest ${winnerDesc} ${fattestCats[0].lbs} lbs and ${fattestCats[0].oz} oz.`.output);
-    setTimeout(() => console.log("Opening cat profile..."), 2000);
-    setTimeout(() => {
-      for(const fattestCat of fattestCats){
-        opener(fattestCat.url);
-      }
-    }, 4000);
+
+    const highestWeight = _(cats).map("weight").max();
+    const fattestCats = _.filter(cats, {weight: highestWeight});
+    const names = _.map(fattestCats, "name");
+    const tie = fattestCats.length > 1;
+
+    const introText = (tie ? "The fattest cats are" : "The fattest cat is").yellow.bold;
+    const nameText = (tie ? `${names.slice(0, -1).join(", ")} and ${_.last(names)}` : names[0]).green.underline.bold;
+    const descriptionText = (tie ? "They each weigh" : (fattestCats[0].isFemale ? "She weighs" : "He weighs")).yellow.bold;
+    const weightText = (`${fattestCats[0].lbs} lbs and ${fattestCats[0].oz} oz.`).yellow.bold;
+    const openText = (tie ? "Opening cat profiles..." : "Opening cat profile...").yellow.bold;
+
+    console.log(`${introText} ${nameText}. ${descriptionText} ${weightText} ${openText}`);
+    setTimeout(() => _(fattestCats).map("url").each(opener), 3000);
   });
